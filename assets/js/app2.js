f213b38c6b59e9e632e8a3942ff1a0675199c814
Ben Snyder <60329801+BenSnyder90@users.noreply.github.com>
2020-06-09 22:23:59 -0400

    Update app2.js

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -3,12 +3,12 @@ var map = L.map('split').setView([23.140, -101.887], 5);
       map.createPane('left');
       map.createPane('right');
 
-      var newark = 'assets/images/map2019.png',
+      var heatmap2019 = 'assets/images/map2019.png',
       imageBounds = [[7.9409, -131.1589], [29.2144, -82.6558]];
-      var leftLayer = L.imageOverlay(newark, imageBounds, {pane: 'left'}).addTo(map);
+      var leftLayer = L.imageOverlay(heatmap2019, imageBounds, {pane: 'left'}).addTo(map);
 
-      var atlantis = 'assets/images/map2020.png',
+      var heatmap2020 = 'assets/images/map2020.png',
       imageBounds = [[7.9409, -131.1589], [29.2144, -82.6558]];
-      var rightLayer = L.imageOverlay(atlantis, imageBounds, {pane: 'right'}).addTo(map);
+      var rightLayer = L.imageOverlay(heatmap2020, imageBounds, {pane: 'right'}).addTo(map);
 
       L.control.splitMap(leftLayer, rightLayer).addTo(map);
