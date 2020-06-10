ab1cfa3ea0308cd23f871951070e185dba052d70
Ben Snyder <60329801+BenSnyder90@users.noreply.github.com>
2020-06-09 22:23:35 -0400

    Update app2.js

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -1,13 +1,13 @@
-var map = L.map('map-id').setView([23.140, -101.887], 5);
+var map = L.map('split').setView([23.140, -101.887], 5);
 
       map.createPane('left');
       map.createPane('right');
 
-      var newark = 'map2019.png',
+      var newark = 'assets/images/map2019.png',
       imageBounds = [[7.9409, -131.1589], [29.2144, -82.6558]];
       var leftLayer = L.imageOverlay(newark, imageBounds, {pane: 'left'}).addTo(map);
 
-      var atlantis = 'map2020.png',
+      var atlantis = 'assets/images/map2020.png',
       imageBounds = [[7.9409, -131.1589], [29.2144, -82.6558]];
       var rightLayer = L.imageOverlay(atlantis, imageBounds, {pane: 'right'}).addTo(map);
 
